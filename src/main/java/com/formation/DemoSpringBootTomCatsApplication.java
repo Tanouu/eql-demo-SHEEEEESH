2f08a185f7ace35f3be6251def637655a4fec6af
Tanouu <ethanpacheco.t@gmail.com>
2024-04-18 14:55:12 +0200

    first commit

---
+++ src/main/java/com/formation/DemoSpringBootTomCatsApplication.java
@@ -0,0 +1,14 @@
+package com.formation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+@SpringBootApplication
+public class DemoSpringBootTomCatsApplication extends SpringBootServletInitializer {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoSpringBootTomCatsApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/formation/web/DemoController.java
@@ -0,0 +1,14 @@
+package com.formation.web;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DemoController {
+
+    @GetMapping("/hello")
+    public String getHello() {
+        return "<h3>Hello World!</h3>";
+
+    }
+}
